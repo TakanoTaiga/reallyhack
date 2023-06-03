13d0c033545f58bbbcaa4c74cf0a88db6c278a2e
Taiga Takano <53041471+TakanoTaiga@users.noreply.github.com>
2023-06-03 18:52:46 +0900

    Update

--- reading_hack.py
+++ reading_hack.py
@@ -8,71 +8,16 @@ def find_mouse_pointer_for_debug():
 
 def mouse_click(x,y):
     pag.moveTo(x,y)
-    time.sleep(0.1)
+    time.sleep(0.05)
     pag.click()
 
 
 ## start lesson 742,280
 def base_lesson():
     mouse_click(742,280)
-    time.sleep(1)
+    time.sleep(0.5)
     for _ in range(10):
         mouse_click(842,1026)
-        time.sleep(0.5)
-    mouse_click(764,1077)
-
-    for _ in range(10):
-        mouse_click(254,979)
-        mouse_click(361,1015)
-        mouse_click(326,1051)
-        mouse_click(307,1084)
-    mouse_click(764,1077) #next
-
-def test_train():
-    mouse_click(749,443)
-    mouse_click(764,1077) #next
-    for _ in range(10):
-        mouse_click(385,1095)
-
-        mouse_click(764,1077)
-        mouse_click(553,792)
-
-    mouse_click(337,724)
-
-    for _ in range(10):
-        mouse_click(385,1095)
-
-        mouse_click(764,1077)
-        mouse_click(553,792)
-
-## MAIN
-#find_mouse_pointer_for_debug()
-mouse_click(528,808)
-base_lesson()
-test_train()
-
-        mouse_click(326,1051)
-        mouse_click(326,1051)import pyautogui as pag
-import time
-
-def find_mouse_pointer_for_debug():
-    while True:
-        print(pag.position())
-        time.sleep(0.5)
-
-def mouse_click(x,y):
-    pag.moveTo(x,y)
-    time.sleep(0.1)
-    pag.click()
-
-
-## start lesson 742,280
-def base_lesson():
-    mouse_click(742,280)
-    time.sleep(1)
-    for _ in range(10):
-        mouse_click(842,1026)
-        time.sleep(0.5)
     mouse_click(764,1077)
 
     for _ in range(10):
